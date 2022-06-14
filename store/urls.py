af10ef8d1a68d51ff4e105f2aaa3706e721fc99d
khuram2025 <khuram2025@gmail.com>
2022-06-14 16:24:40 +0300

    working on views

---
+++ store/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'store'
+
+
+urlpatterns = [
+    path('', views.all_products, name='all_products'),
+    # path('<int:product_id>/', views.detail, name='detail'),
+    # path('<int:product_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    # path('<int:product_id>/remove_from_cart/', views.remove_from_cart, name='remove_from_cart'),
+    # path('cart/', views.cart, name='cart'),
+    # path('checkout/', views.checkout, name='checkout'),
+    # path('order_complete/', views.order_complete, name='order_complete'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+# Create your views here.   
+def all_products(request):
+    return render(request, 'store/all_products.html')
+
