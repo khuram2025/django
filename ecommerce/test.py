c926ccc8497ef8f605768adba95ead72935e06f1
khuram2025 <khuram2025@gmail.com>
2022-06-14 15:01:55 +0300

    1st

--- ecommerce/test.py
+++ ecommerce/test.py
@@ -5,4 +5,7 @@ From Office PC dfdfsdfsdfsdf
 sdfsdfsdfsdfsdfsdfdsf
 =======
 From Office PC 
->>>>>>> 8856c60120108b553132f9b042135a15d5e325e1
+>>>>>>> 8856c60120108b553132f9b042135a1
+
+this is the comment from office pc
+
