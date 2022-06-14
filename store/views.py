f12d5c2bef892056026b532898ef2c0514457ccc
khuram <khuram2025@gmail.com>
2022-06-14 20:03:09 +0300

    done home laptop

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
+from itertools import product
 from django.shortcuts import render
 from .models import Product, Category
 
 # Create your views here.   
 def all_products(request):
-    return render(request, 'store/all_products.html')
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
 
